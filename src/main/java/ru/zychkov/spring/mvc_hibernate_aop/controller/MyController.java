3804b747cda0f150903f1ee3a2942afb974e617e
Zychkov <48855208+Zychkov@users.noreply.github.com>
2021-07-18 18:43:28 +0700

    finish mvc_hibernate_aop

--- src/main/java/ru/zychkov/spring/mvc_hibernate_aop/controller/MyController.java
+++ src/main/java/ru/zychkov/spring/mvc_hibernate_aop/controller/MyController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.zychkov.spring.mvc_hibernate_aop.entity.Employee;
 import ru.zychkov.spring.mvc_hibernate_aop.service.EmployeeService;
 
@@ -32,10 +33,26 @@ public class MyController {
         return "employee-info";
     }
 
-    @RequestMapping("saveEmployee")
+    @RequestMapping("/saveEmployee")
     public String saveEmployee(@ModelAttribute("employee") Employee employee) {
         employeeService.saveEmployee(employee);
 
         return "redirect:/";
     }
+
+    @RequestMapping("/updateInfo")
+    public String updateEmployee(@RequestParam("empId") int id, Model model) {
+        Employee employee = employeeService.getEmployee(id);
+        model.addAttribute("employee", employee);
+
+        return "employee-info";
+    }
+
+    @RequestMapping("/deleteEmployee")
+    public String deleteEmployee(@RequestParam("empId") int id) {
+        employeeService.deleteEmployee(id);
+
+        return "redirect:/";
+    }
+
 }
